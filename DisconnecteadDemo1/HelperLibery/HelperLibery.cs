91bd2d4f10556735076c8028a3f8edc71dc0f40d
monika24794 <33451089+monika24794@users.noreply.github.com>
2024-06-22 17:37:45 +0530

    Add files via upload

---
+++ DisconnecteadDemo1/HelperLibery/HelperLibery.cs
@@ -0,0 +1,28 @@
+﻿using BuinessLogic_Library;
+using DataAccessLibrery;
+
+namespace HelperLibery
+{
+    public class CustomerHelper
+    {
+        CustomerDAl dal = new CustomerDAl();
+        public List<CustomerBAL> GetCustomers()
+        {
+            List<CustomerBAL> custlist = dal.GetCustomers();
+            return custlist;
+        }
+        public void insertCustomer(CustomerBAL cust)
+        {
+            dal.insertCustomer(cust);
+        }
+        public void UpdateCustomer(CustomerBAL cust)
+        {
+            dal.updateCustomer(cust);
+        }
+        public void deleteCustomer(CustomerBAL cust)
+        {
+            dal.deleteCustomer(cust);
+        }
+
+        }
+}
