a72603ed48da966472e4da5058e1d2860b76b0e5
monika24794 <33451089+monika24794@users.noreply.github.com>
2024-06-22 16:57:38 +0530

    Add files via upload

---
+++ AsyncAndAwaitDemo/AsyncAndAwaitDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace AsyncAndAwaitDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            print1to10();
+            printHello();
+             Task<int> ans=multiplayNo(2, 3);
+            Console.WriteLine("ANSWER =  " +ans.Result);
+        }
+        public static async void print1to10()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(i);
+                    Task.Delay(10);
+                }
+            });
+        }
+        public static async void printHello()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine("HELLO");
+                    Task.Delay(10);
+                }
+            });
+        }
+
+        public static async Task<int> multiplayNo(int i,int j)
+        {
+            int ans = 0;
+
+            await Task.Run(() =>
+            {
+
+                for (int j1 = 0; j1 < 10; j1++)
+                {
+                    ans = i*j * 10;
+
+                }
+            });
+
+            return ans;
+        }
+    }
+}
+
